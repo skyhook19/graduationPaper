97ea91147084e6fcd55645ce94708a8168459969
Kairat Sagaleev <bronfirstring@gmail.com>
2017-05-11 13:59:50 +0400

    added thymeleaf and bootstrap

--- src/main/java/com/sagaleev/controllers/BaseController.java
+++ src/main/java/com/sagaleev/controllers/BaseController.java
@@ -1,13 +1,14 @@
 package com.sagaleev.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BaseController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/")
     public String showHelloWorld(){
-        return "hello-world";
+        return "index";
     }
 }
