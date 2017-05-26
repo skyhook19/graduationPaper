31c98d91537e505ae96999ef579cab49888572e2
Kairat Sagaleev <bronfirstring@gmail.com>
2017-05-26 10:59:11 +0400

    fixed training

--- src/main/java/com/sagaleev/WeatherDiseaseNetwork.java
+++ src/main/java/com/sagaleev/WeatherDiseaseNetwork.java
@@ -4,7 +4,6 @@ import com.sagaleev.domain.model.AmbulanceCallStats;
 import com.sagaleev.domain.model.WeatherStats;
 import com.sagaleev.service.AmbulanceCallStatsService;
 import com.sagaleev.service.WeatherStatsService;
-import org.apache.commons.lang3.ArrayUtils;
 import org.encog.engine.network.activation.ActivationSigmoid;
 import org.encog.ml.data.MLData;
 import org.encog.ml.data.MLDataPair;
@@ -15,14 +14,10 @@ import org.encog.neural.networks.BasicNetwork;
 import org.encog.neural.networks.layers.BasicLayer;
 import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation;
 import org.encog.util.arrayutil.NormalizationAction;
-import org.encog.util.arrayutil.NormalizeArray;
 import org.encog.util.arrayutil.NormalizedField;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.Month;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
@@ -31,25 +26,29 @@ public class WeatherDiseaseNetwork {
     private final WeatherStatsService weatherStatsService;
     private final AmbulanceCallStatsService ambulanceCallStatsService;
 
+    private static final int NORMALIZED_HIGH = 1;
+    private static final int NORMALIZED_LOW = 0;
+
     @Autowired
     public WeatherDiseaseNetwork(WeatherStatsService weatherStatsService, AmbulanceCallStatsService ambulanceCallStatsService) {
         this.weatherStatsService = weatherStatsService;
         this.ambulanceCallStatsService = ambulanceCallStatsService;
     }
 
+
     public double[] computeNetwork(double[][] weatherInput) {
         BasicNetwork network = train();
 
-        double[][] normalizedWeatherInput = normalizeArray(weatherInput);
+ /*       double[][] normalizedWeatherInput = normalizeArray(weatherInput);
         double[][] ideal = null;
 
         MLDataSet set = new BasicMLDataSet(normalizedWeatherInput, ideal);
 
-        System.out.println("Neural Network Results: ");
+        System.out.println("Neural Network Results: ");*/
 
         double[] result = new double[15];
 
-        for (MLDataPair pair : set) {
+        /*for (MLDataPair pair : set) {
             final MLData output = network.compute(pair.getInput());
 
             System.out.print("Weather data input: ");
@@ -64,52 +63,53 @@ public class WeatherDiseaseNetwork {
                 System.out.print(", " + denormalize(outputValue));
                 result[i] = denormalize(outputValue);
             }
-        }
+        }*/
 
         return result;
     }
 
-
-
     private BasicNetwork train() {
-        double[][] weatherArray = getWeatherData();
-        double[][] ambulanceCallResultArray = getAmbulanceCallsData();
+        double[][] weatherDataSet = getTrainingWeatherDataSet();
+        double[][] ambulanceCallDataSet = getTrainingAmbulanceCallsDataSet();
+        printDataArray(weatherDataSet, "weather data");
+        printDataArray(ambulanceCallDataSet, "ambulanceCallResultArray");
 
-        printDataArray(weatherArray, "weather data");
-        printDataArray(ambulanceCallResultArray, "ambulanceCallResultArray");
+        double[][] normalizedWeatherDataSet = new double[weatherDataSet.length][];
+        for (int i = 0; i < weatherDataSet.length; i++) {
+            normalizedWeatherDataSet[i] = normalizeArray(weatherDataSet[i]);
+        }
 
-        double[][] normalizedWeatherArray = normalizeArray(weatherArray);
-        double[][] normalizedAmbulanceCallArray = normalizeArray(ambulanceCallResultArray);
+        double[][] normalizedAmbulanceCallDataSet = new double[ambulanceCallDataSet.length][];
+        for (int i = 0; i < ambulanceCallDataSet.length; i++) {
+            normalizedAmbulanceCallDataSet[i] = normalizeArray(ambulanceCallDataSet[i]);
+        }
 
-        //набор данных для обучения
-        MLDataSet trainingSet = new BasicMLDataSet(normalizedWeatherArray, normalizedAmbulanceCallArray);
+        MLDataSet trainingSet = new BasicMLDataSet(normalizedWeatherDataSet, normalizedAmbulanceCallDataSet);
 
-        //инициализация нейросети
         BasicNetwork network = new BasicNetwork();
         network.addLayer(new BasicLayer(null, true, 8));
         network.addLayer(new BasicLayer(new ActivationSigmoid(), true, 16));
         network.addLayer(new BasicLayer(new ActivationSigmoid(), true, 32));
         network.addLayer(new BasicLayer(new ActivationSigmoid(), true, 64));
         network.addLayer(new BasicLayer(new ActivationSigmoid(), true, 32));
-        network.addLayer(new BasicLayer(new ActivationSigmoid(), true, 16));
         network.addLayer(new BasicLayer(new ActivationSigmoid(), false, 15));
         network.getStructure().finalizeStructure();
 
-        //создаю обьект, обуч нейросеть
         MLTrain train = new ResilientPropagation(network, trainingSet);
 
         //обучение, до тех пор пока ошибка не станет меньше n
+        //TODO разбить dataSet на данные для обучения(2011-2014) и данные для проверки(2015) и вычислять еще ActualError
         int epoch = 1;
         do {
             train.iteration();
             System.out.println("Epoch #" + epoch + "Error:" + train.getError());
             epoch++;
-        } while (train.getError() > 0.02);
+        } while (train.getError() > 0.01);
 
         return network;
     }
 
-    private double[][] getWeatherData() {
+    private double[][] getTrainingWeatherDataSet() {
         List<WeatherStats> weatherStats = weatherStatsService.getAll();
         double[][] result = new double[weatherStats.size()][];
 
@@ -131,7 +131,7 @@ public class WeatherDiseaseNetwork {
         return result;
     }
 
-    private double[][] getAmbulanceCallsData() {
+    private double[][] getTrainingAmbulanceCallsDataSet() {
         List<AmbulanceCallStats> ambulanceCallStats = ambulanceCallStatsService.getAll();
         double[][] result = new double[ambulanceCallStats.size()/15][];
 
@@ -161,56 +161,55 @@ public class WeatherDiseaseNetwork {
 
     }
 
-    private double[][] normalizeArray(double[][] input){
-        double[][] result = new double[input.length][input[0].length];
+    private double[] normalizeArray(double[] arr){
+        double[] normalizedInputArray = new double[arr.length];
+        double min = getMinInArray(arr);
+        double max = getMaxInArray(arr);
 
-        //TODO возможно, проблема в том, что я ищу максимум среди всех строк, а нужно искать для каждой строки свой максимум
-
-        double min = getMin(input);
-        double max = getMax(input);
-
-        for (int i = 0; i < input.length; i++) {
-            for (int j = 0; j < input[i].length; j++) {
-                result[i][j] = normalize(max, min, input[i][j]);
-            }
+        for (int i = 0; i < arr.length; i++) {
+            normalizedInputArray[i] = normalize(max, min, arr[i]);
         }
+        return normalizedInputArray;
+    }
 
-        return result;
+    private double[] denormalizeArray(double[] arr){
+        double[] denormalizedInputArray = new double[arr.length];
+        double min = getMinInArray(arr);
+        double max = getMaxInArray(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            denormalizedInputArray[i] = denormalize(max, min, arr[i]);
+        }
+        return denormalizedInputArray;
     }
 
     private double normalize(double actualHigh, double actualLow, double valueToNormalize) {
-        NormalizedField normalizedField = new NormalizedField(NormalizationAction.Normalize, "normalize", actualHigh, actualLow, 1, -1);
+        NormalizedField normalizedField = new NormalizedField(NormalizationAction.Normalize, "normalize", actualHigh, actualLow, NORMALIZED_HIGH, NORMALIZED_LOW);
         return normalizedField.normalize(valueToNormalize);
     }
 
-    private double denormalize(double normalizedValue) {
-        NormalizedField normalizedField = new NormalizedField();
+    private double denormalize(double actualHigh, double actualLow, double normalizedValue) {
+        NormalizedField normalizedField = new NormalizedField(NormalizationAction.Normalize, "denormalize", actualHigh, actualLow, NORMALIZED_HIGH, NORMALIZED_LOW);
         return normalizedField.deNormalize(normalizedValue);
     }
 
-    private double getMax(double[][] arr){
-        double max = arr[0][0];
-
-        for (int i = 0; i < arr.length; i++){
-            for (int j = 0; j < arr[i].length; j++){
-                if(arr[i][j] > max) max = arr[i][j];
-            }
+    private double getMaxInArray(double[] arr){
+        double max = arr[0];
+        for (int i = 1; i < arr.length; i++){
+            if(arr[i] > max) max = arr[i];
         }
-
         return max;
     }
 
-    private double getMin(double[][] arr){
-        double min = arr[0][0];
-
-        for (int i = 0; i < arr.length; i++){
-            for (int j = 0; j < arr[i].length; j++){
-                if(arr[i][j] < min) min = arr[i][j];
-            }
+    private double getMinInArray(double[] arr){
+        double min = arr[0];
+        for (int i = 1; i < arr.length; i++){
+            if(arr[i] < min) min = arr[i];
         }
-
         return min;
     }
+
+    //TODO что еще попробовать: попробовать передавать в качестве actualHigh = max * 1.25
 }
 
 
